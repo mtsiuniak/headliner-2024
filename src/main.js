44c084234e31d324a056e67919b7c3f802718813
Alex <112724167+klimbass@users.noreply.github.com>
2024-03-17 11:32:43 +0200

    footer_with_two_bugs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
 import 'swiper/css';
+import axios from "axios"
+import postRequestPortfolioApi from './js/postRequestPortfolioApi';
 // hero background-image 
 const backgroundImageHero = document.querySelector('#hero-section');
 function heroBackgroundImage() {
@@ -17,3 +19,61 @@ function heroBackgroundImage() {
     }
 heroBackgroundImage();
 //  /hero background-image 
+
+
+//work_together 
+
+
+
+const form = document.querySelector('.footer-form')
+ const emailInput = document.querySelector('input[name="email"]')
+form.addEventListener('submit', handSubmit)
+
+function handSubmit(event) {
+    event.preventDefault()
+    const userEmail = event.target.email.value.trim()
+    const userComment = event.target.comments.value.trim()
+    if (userEmail || userComment) {
+        console.log(`${userEmail}: ${userComment}`);
+        const options = new URLSearchParams({
+            email: userEmail,
+            comment: userComment,
+        })
+        const jsonOptions = options.json()
+        console.log(typeof jsonOptions);
+        postRequestPortfolioApi(jsonOptions)
+            .then(data => {
+            alert(`ok ${data}`)
+            })
+            .catch(er => {
+            alert(er)
+        })
+    };
+
+    
+}
+
+
+
+emailInput.addEventListener('focus', () => {
+    if (form.classList.contains('success-email')) {
+        form.classList.remove('success-email')
+    }
+    if (form.classList.contains('failed-email')) {
+        form.classList.remove('failed-email')
+    }
+})
+
+
+emailInput.addEventListener('blur', () => {
+    if (emailInput.checkValidity()) {
+        form.classList.add('success-email')
+    } else {
+        form.classList.add('failed-email')
+        
+    }
+
+})
+
+//  /work_together
+
