e475fe1384bec228bd7a0cfd655220c40b474c1d
taniaig233 <145375407+taniaig233@users.noreply.github.com>
2024-03-18 11:27:55 +0200

    upd1

---
+++ src/faq.js
@@ -0,0 +1,7 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+
+const accordion = new Accordion('.accordion-container', {
+  showMultiple: true,
+});
--- src/main.js
+++ src/main.js
@@ -75,3 +75,4 @@ function renderReviews(review) {
     })
     .join('');
 }
+
