331f9ee28c31a5010f2c6e236be3363555d58556
taniaig233 <145375407+taniaig233@users.noreply.github.com>
2024-03-18 20:44:59 +0200

    upd3 accordion

--- src/faq.js
+++ src/faq.js
@@ -1,6 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const accordion = new Accordion('.accordion-container', {
+const accordion2 = new Accordion('#accordion2', {
   showMultiple: true,
 });
